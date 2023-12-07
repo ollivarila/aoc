e3e357fe0dcf30889f983033ec7fa05abd346723
Olli Varila <olli.varila@gmail.com>
2023-12-07 12:40:39 +0200

    day6 base

---
+++ aoc23/day6/src/bin/part1.rs
@@ -0,0 +1,29 @@
+
+fn main() {
+    println!("Hello world");
+}
+
+fn parse() {
+    todo!()
+}
+
+fn solution(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    
+    #[test]
+    fn test_input() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        let answer = solution(input);
+        assert_eq!(answer, 288);
+    }
+    
+}
+
+
