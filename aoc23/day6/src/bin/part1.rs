da442f9d98571fc6ca61af7262aea59786cfca33
Olli <olli.varila@gmail.com>
2023-12-07 15:57:12 +0200

    stuff

--- aoc23/day6/src/bin/part1.rs
+++ aoc23/day6/src/bin/part1.rs
@@ -1,10 +1,31 @@
+use nom::IResult;
+use nom::bytes::complete::{tag, take_until};
+use nom::multi::{separated_list1};
+use nom::sequence::{delimited, preceded};
+use nom::character::complete::{u32, newline, multispace1};
 
 fn main() {
-    println!("Hello world");
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        let res = parse(input);
+        dbg!(res);
 }
 
-fn parse() {
-    todo!()
+fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let (_, times) = parse_time(input).unwrap();
+    let (_, distances) = parse_distance(input).unwrap();
+    (times, distances)
+}
+
+fn parse_time(input: &str) -> IResult<&str, Vec<u32>> {
+    let (remaining, parsed) = tag("Time:")(input)?;
+    let (remaining, parsed) = delimited(multispace1, separated_list1(multispace1, u32), newline)(remaining)?;
+
+    Ok((remaining, parsed))
+}
+
+fn parse_distance(input: &str) -> IResult<&str, Vec<u32>> {
+    preceded(tag("Distance:"), separated_list1(multispace1, u32))(input)
 }
 
 fn solution(input: &str) -> u64 {
