5a435661148487933d4a458289455ecfe4689c82
Olli Varila <olli.varila@gmail.com>
2023-12-12 21:43:25 +0200

    part2

--- aoc23/day8/src/bin/part1.rs
+++ aoc23/day8/src/bin/part1.rs
@@ -1,8 +1,12 @@
-use std::collections::HashMap;
-use nom::{sequence::{separated_pair, delimited, preceded, terminated}, character::complete::{multispace1, space1, anychar, alpha1}, bytes::complete::take_until};
-use nom::IResult;
-use nom::bytes::complete::take;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take;
+use nom::IResult;
+use nom::{
+    bytes::complete::take_until,
+    character::complete::{alpha1, anychar, multispace1, space1},
+    sequence::{delimited, preceded, separated_pair, terminated},
+};
+use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("../../input.txt");
@@ -11,36 +15,48 @@ fn main() {
 }
 
 fn solution(input: &str) -> u64 {
-    let (rules, map, (start, target))= parse(input);
+    let (rules, map, (start, target)) = parse(input);
     dbg!(&rules.len(), &start, &target);
-    let instructions = Instructions { instructions: map, rules };
+    let instructions = Instructions {
+        instructions: map,
+        rules,
+    };
     instructions.find_target("AAA", "ZZZ")
 }
 
 fn parse(input: &str) -> (Vec<char>, HashMap<&str, (String, String)>, (String, String)) {
-    let (remaining, rules ) = parse_rules(input).unwrap();
+    let (remaining, rules) = parse_rules(input).unwrap();
 
-    let results: Vec<(&str, (&str, &str))> = remaining.lines().map(|l| {
-        parse_line(l).unwrap().1
-    }).collect();
-    
+    let results: Vec<(&str, (&str, &str))> = remaining
+        .lines()
+        .map(|l| parse_line(l).unwrap().1)
+        .collect();
+
     let mut map = HashMap::new();
-    
+
     for (key, (left, right)) in results.clone() {
         map.insert(key, (String::from(left), String::from(right)));
     }
     let start = results.first().unwrap().0;
     let target = results.last().unwrap().0;
-    
+
     (rules, map, (start.to_string(), target.to_string()))
 }
 
 fn parse_line(line: &str) -> IResult<&str, (&str, (&str, &str))> {
-    separated_pair(take(3usize), delimited(space1, anychar, space1), parse_tuple)(line)
+    separated_pair(
+        take(3usize),
+        delimited(space1, anychar, space1),
+        parse_tuple,
+    )(line)
 }
 
 fn parse_tuple(input: &str) -> IResult<&str, (&str, &str)> {
-    separated_pair(preceded(tag("("), alpha1), tag(", "), terminated(alpha1, tag(")")))(input)
+    separated_pair(
+        preceded(tag("("), alpha1),
+        tag(", "),
+        terminated(alpha1, tag(")")),
+    )(input)
 }
 
 fn parse_rules(input: &str) -> IResult<&str, Vec<char>> {
@@ -61,7 +77,7 @@ impl Instructions<'_> {
     fn get_right(&self, key: &str) -> String {
         self.instructions.get(key).unwrap().1.clone()
     }
-    
+
     fn find_target(&self, start: &str, target: &str) -> u64 {
         self.find_target_rec(start.to_string(), target, 1)
     }
@@ -72,10 +88,10 @@ impl Instructions<'_> {
             match rule {
                 'L' => {
                     current = self.get_left(&current);
-                },
+                }
                 'R' => {
                     current = self.get_right(&current);
-                },	
+                }
                 _ => panic!("Unknown rule"),
             }
             if current == target {
@@ -89,7 +105,7 @@ impl Instructions<'_> {
 
 #[cfg(test)]
 mod tests {
-    
+
     use super::*;
 
     #[test]
@@ -106,7 +122,7 @@ ZZZ = (ZZZ, ZZZ)";
         let answer = solution(input);
         assert_eq!(answer, 2);
     }
-    
+
     #[test]
     fn test_input2() {
         let input = "LLR
@@ -116,6 +132,5 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
         let answer = solution(input);
         assert_eq!(answer, 6)
-        
     }
 }
--- aoc23/day8/src/bin/part2.rs
+++ aoc23/day8/src/bin/part2.rs
@@ -1,54 +1,88 @@
-use indicatif::ProgressIterator;
-use std::collections::HashMap;
-use nom::{sequence::{separated_pair, delimited, preceded, terminated}, character::complete::{multispace1, space1, anychar}, bytes::complete::take_until};
-use nom::IResult;
-use nom::bytes::complete::take;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take;
+use nom::IResult;
+use nom::{
+    bytes::complete::take_until,
+    character::complete::{anychar, multispace1, space1},
+    sequence::{delimited, preceded, separated_pair, terminated},
+};
+use std::{collections::HashMap, vec};
 
 fn main() {
-    let input = include_str!("../../input2.txt");
+    let input = include_str!("../../input.txt");
     let ans = solution(input);
     println!("{:?}", ans);
 }
 
-fn solution(input: &str) -> u64 {
-    let (rules, map, starts)= parse(input);
-    let instructions = Instructions { instructions: map, rules };
-    instructions.find_target(starts)
+fn solution(input: &str) -> u128 {
+    let (rules, map, starts) = parse(input);
+    let instructions = Instructions {
+        instructions: map,
+        rules,
+    };
+    let all = instructions.find_targets(starts);
+    lcm(&*all.into_boxed_slice())
 }
 
 fn parse(input: &str) -> (Vec<char>, HashMap<&str, (String, String)>, Vec<String>) {
-    let (remaining, rules ) = parse_rules(input).unwrap();
+    let (remaining, rules) = parse_rules(input).unwrap();
 
-    let results: Vec<(&str, (&str, &str))> = remaining.lines().map(|l| {
-        parse_line(l).unwrap().1
-    }).collect();
-    
+    let results: Vec<(&str, (&str, &str))> = remaining
+        .lines()
+        .map(|l| parse_line(l).unwrap().1)
+        .collect();
+
     let mut map = HashMap::new();
-    
+
     for (key, (left, right)) in results.clone() {
         map.insert(key, (String::from(left), String::from(right)));
     }
     let starts = map
         .iter()
         .filter(|item| item.0.ends_with("A"))
-        .map(|item| item.0.to_string()).collect();
-    
+        .map(|item| item.0.to_string())
+        .collect();
+
     (rules, map, starts)
 }
 
 fn parse_line(line: &str) -> IResult<&str, (&str, (&str, &str))> {
-    separated_pair(take(3usize), delimited(space1, anychar, space1), parse_tuple)(line)
+    separated_pair(
+        take(3usize),
+        delimited(space1, anychar, space1),
+        parse_tuple,
+    )(line)
 }
 
 fn parse_tuple(input: &str) -> IResult<&str, (&str, &str)> {
-    separated_pair(preceded(tag("("), take(3usize)), tag(", "), terminated(take(3usize), tag(")")))(input)
+    separated_pair(
+        preceded(tag("("), take(3usize)),
+        tag(", "),
+        terminated(take(3usize), tag(")")),
+    )(input)
 }
 
 fn parse_rules(input: &str) -> IResult<&str, Vec<char>> {
     let (r, rules) = take_until("\n\n")(input)?;
     let (r, _) = multispace1(r)?;
     Ok((r, rules.chars().collect()))
+}
+
+fn lcm(nums: &[u128]) -> u128 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
 }
 
 struct Instructions<'a> {
@@ -63,45 +97,39 @@ impl Instructions<'_> {
     fn get_right(&self, key: &str) -> String {
         self.instructions.get(key).unwrap().1.clone()
     }
-    
-    fn find_target(&self, starts: Vec<String>) -> u64 {
-        self.find_target_rec(starts, 1)
+
+    fn find_targets(&self, start: Vec<String>) -> Vec<u128> {
+        let mut result = vec![];
+        for key in start {
+            let value = self.find_target(key);
+            result.push(value as u128);
+        }
+        result
     }
-    fn find_target_rec(&self, start: Vec<String>, count: u64) -> u64 {
-        let mut currents = start.clone();
-        let mut count = count;
+
+    fn find_target(&self, start: String) -> u64 {
+        let mut count = 1;
+        let mut current = start;
         loop {
+            // Should always find something
             for rule in &self.rules {
-                let thing: Vec<String> = currents.iter().map(|val| {
-                    match rule {
-                        'L' => {
-                            self.get_left(val)
-                        },
-                        'R' => {
-                            self.get_right(val)
-                        },
-                        _ => panic!("Unknown rule")
-                    }
-                }).collect();
-                
-                let at_end = thing.iter().all(|s| s.ends_with("Z"));
-                if at_end {
+                current = match rule {
+                    'L' => self.get_left(&current),
+                    'R' => self.get_right(&current),
+                    _ => panic!("Unexpected rule"),
+                };
+                if current.ends_with("Z") {
                     return count;
                 }
-                else {
-                    currents = thing;
-                    count += 1;
-                }
-            };
+                count += 1;
+            }
         }
     }
-    
 }
-
 
 #[cfg(test)]
 mod tests {
-    
+
     use super::*;
 
     #[test]
@@ -119,5 +147,4 @@ XXX = (XXX, XXX)";
         let answer = solution(input);
         assert_eq!(answer, 6);
     }
-    
 }
