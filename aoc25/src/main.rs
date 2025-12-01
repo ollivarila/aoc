d0be3b810159cae11c3ae2f80723aa4d6b92cd37
Olli Varila <olli.varila@gmail.com>
2025-12-01 18:24:53 +0200

    Day 1

---
+++ aoc25/src/main.rs
@@ -0,0 +1,87 @@
+fn main() {
+    solve_pt1();
+    solve_pt2();
+}
+
+const INPUT: &str = include_str!("../input/d1.txt");
+
+fn solve_pt2() {
+    let mut count = 0;
+    let mut rot: i32 = 50;
+
+    for mut line in INPUT.lines().map(|l| l.chars()) {
+        let op = line.next().unwrap();
+        let total = line.as_str().parse::<i32>().unwrap();
+        let overflow_times = total / 100;
+        let n = total % 100;
+        if overflow_times == 0 {
+            assert_eq!(n, total);
+        }
+        count += overflow_times;
+
+        assert!(rot < 100 && rot >= 0);
+        assert!(n <= 100 && n >= 0);
+        match op {
+            'L' => {
+                let after = rot - n;
+                if after < 0 {
+                    if rot != 0 {
+                        count += 1;
+                    }
+                    rot = after + 100;
+                } else {
+                    rot = after;
+                    if after == 0 && n != 0 {
+                        count += 1;
+                    }
+                }
+            }
+            'R' => {
+                let after = rot + n;
+                if after > 99 {
+                    count += 1;
+                    rot = after - 100;
+                } else {
+                    rot = after;
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    println!("{count}");
+}
+
+fn solve_pt1() {
+    let mut count = 0;
+    let mut rot: i32 = 50;
+
+    for mut line in INPUT.lines().map(|l| l.chars()) {
+        let op = line.next().unwrap();
+        let n = line.as_str().parse::<i32>().unwrap() % 100;
+        match op {
+            'L' => {
+                if rot - n >= 0 {
+                    rot -= n;
+                } else {
+                    let n = n - rot;
+                    rot = 100 - n;
+                }
+            }
+            'R' => {
+                if rot + n < 100 {
+                    rot += n;
+                } else {
+                    rot = n + rot - 100;
+                }
+            }
+            _ => panic!(),
+        }
+
+        if rot == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
